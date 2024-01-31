497338a6bc20d1568fae7f285837b6c64bbfea54
K. Tharun Selvam <125911256+tharun-selvam@users.noreply.github.com>
2024-01-31 19:00:46 +0530

    Update setup.py
    Reverted the changed

--- setup.py
+++ setup.py
@@ -96,9 +96,9 @@ extra_packages = extra_no_roms + [  # noqa: RUF005
 
 
 setup(
-    name="stable_baselines3_changed",
-    packages=[package for package in find_packages() if package.startswith("stable_baselines3_changed")],
-    package_data={"stable_baselines3_changed": ["py.typed", "version.txt"]},
+    name="stable_baselines3",
+    packages=[package for package in find_packages() if package.startswith("stable_baselines3")],
+    package_data={"stable_baselines3": ["py.typed", "version.txt"]},
     install_requires=[
         "gymnasium>=0.28.1,<0.30",
         "numpy>=1.20",
