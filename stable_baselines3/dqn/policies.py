675b5d639f529895be86cb43d71355bb2db01ad5
tharun-selvam <tharunselvam22@gmail.com>
2024-02-01 22:45:20 +0530

    swami

--- stable_baselines3/dqn/policies.py
+++ stable_baselines3/dqn/policies.py
@@ -80,7 +80,7 @@ class QNetwork(BasePolicy):
         features_dim: int,
         net_arch: Optional[List[int]] = None,
         activation_fn: Type[nn.Module] = nn.ReLU,
-        activation_fn_2 = FTA(-1, 1, 2., 0.5),
+        activation_fn_2 = FTA(-1, 1, 2., 0.2),
         normalize_images: bool = True,
     ) -> None:
         super().__init__(
