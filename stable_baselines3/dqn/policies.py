4595111dfb5215b7ceb3daf04f1029a894f5b31e
tharun-selvam <tharunselvam22@gmail.com>
2024-02-02 15:21:19 +0530

    params changed

--- stable_baselines3/dqn/policies.py
+++ stable_baselines3/dqn/policies.py
@@ -80,7 +80,7 @@ class QNetwork(BasePolicy):
         features_dim: int,
         net_arch: Optional[List[int]] = None,
         activation_fn: Type[nn.Module] = nn.ReLU,
-        activation_fn_2 = FTA(-20, 20, 2., 2.),
+        activation_fn_2 = FTA(-20, 20, .05, .05),
         normalize_images: bool = True,
     ) -> None:
         super().__init__(
